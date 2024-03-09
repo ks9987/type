f8111f93cfccfddff45a1b8edde36035c530a8d0
ks9987 <kengo.suzuki@relic.co.jp>
2024-03-10 02:56:50 +0900

    pr

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,69 @@
-import  * as fs from 'fs';
+import { createInterface } from "readline";
+import * as fs from "fs";
 
-const input = fs.readFileSync("/dev/stdin", "utf8");
+let inputs = "";
+let inputArray: string[];
+let currentIndex = 0;
 
-console.log(input);
+let outputBuffer = "";
+
+function next() {
+  return inputArray[currentIndex++];
+}
+function nextNum() {
+  return +next();
+}
+function nextBigInt() {
+  return BigInt(next());
+}
+function nexts(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = next();
+  return arr;
+}
+function nextNums(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = nextNum();
+  return arr;
+}
+function nextBigInts(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = nextBigInt();
+  return arr;
+}
+
+function print(out: string | number | bigint): void;
+function print<T>(out: Array<T>, separator: string): void;
+function print<T>(out: string | number | bigint | Array<T>, separator?: string) {
+  if (Array.isArray(out)) {
+    outputBuffer += out.join(separator);
+  } else {
+    outputBuffer += out;
+  }
+}
+
+function println(out: string | number | bigint): void;
+function println<T>(out: Array<T>, separator: string): void;
+function println<T>(out: string | number | bigint | Array<T>, separator?: string) {
+  if (Array.isArray(out)) {
+    print(out, separator || "");
+  } else {
+    print(out);
+  }
+  print("\n");
+}
+
+function flush() {
+  console.log(outputBuffer);
+}
+
+inputs = fs.readFileSync("/dev/stdin", "utf8");
+inputArray = inputs.split(/\s/);
+main();
+flush();
+
+function main() {
+  const [a, b, c] = nextNums(3);
+  const s = next();
+  println(`${a + b + c} ${s}`);
+}
